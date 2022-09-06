10aa9cdeeee58afcc9b17ca9bbc1b4341132f2ad
William-Guillermo <guillermodelapazw@gmail.com>
2022-09-06 18:41:21 -0500

    Filtros x fechas-Usarios

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -8,12 +8,22 @@ class ClientesAdmin(admin.ModelAdmin):
     # Busqueda de informacion por filtros cuando son muchos campos y se requiere hacer una busqueda
     search_fields=('nombre','email','tfono') 
 
+class ArticulosAdmin(admin.ModelAdmin):
+    #Filtro por seccion
+    list_filter=("seccion",)
+    #list_filter: _ListOrTuple[_ListFilterT]
+
+class PedidosAdmin(admin.ModelAdmin):
+    list_display=('numero','fecha')
+    list_filter=('fecha',)  # menu filtrado de la derecha para buscar por fechas
+    date_hierarchy='fecha'  #no lleva parentesis ni coma al final , filtro migas de Pan
+
 # Register your models here.
 # Para tener disponible desde el panel de administracion la tabla de 
 # se agrega en el registro, la clase generada anteriormente ClientesAdmin
 admin.site.register(Clientes,ClientesAdmin)  
-admin.site.register(Articulos) 
-admin.site.register(Pedidos)  
+admin.site.register(Articulos,ArticulosAdmin) 
+admin.site.register(Pedidos,PedidosAdmin)  
 
 
 
